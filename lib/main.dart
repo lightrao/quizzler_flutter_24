3572dfd7f79554de9a27b7ec98b9f583d31eac33
lightrao <lightrao@hotmail.com>
2024-11-24 21:48:46 +0800

    Checking User Answers

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,12 @@ class _QuizPageState extends State<QuizPage> {
   ];
   int questionNumber = 0;
 
+  List<bool> answers = [
+    false,
+    true,
+    true,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -72,6 +78,13 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 // The user picked true.
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == true) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
+
                 setState(() {
                   questionNumber++;
                 });
@@ -93,6 +106,13 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 // The user picked false.
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == false) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
+
                 setState(() {
                   questionNumber++;
                 });
